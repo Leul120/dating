800167c71c59739cca0723d52bfd9add5991d3ae
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-09-20 01:06:33 -0700

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,5 +3,9 @@ const { registerUser } = require('../controllers/authControllers')
 const router=express.Router()
 
 router.route('/submit-form').post(registerUser)
-
+router.route('/').post((req,res)=>{
+    res.send({
+        message:"hello "
+    })
+})
 module.exports=router
