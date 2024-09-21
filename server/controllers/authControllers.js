80955edb848b791411bb159ded68b27bb04ad5c1
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-09-21 01:52:53 -0700

    Update authControllers.js

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -148,7 +148,7 @@ sharedGoals} = req.body;
         })
         res.status(200).json({status:200,
         message:"Email sent successfully.",
-            token})  
+    })  
     } catch (err) {
         console.log(err)
         res.status(500).json({ message: err.message });
