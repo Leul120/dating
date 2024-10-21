77a4e5d50367cf7305555672a7c725480299c2b3
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-10-21 11:45:58 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import DatingForm from './datingForm';
 import HomePage from './homePage';
 import ReadyToDatePage from './hover';
+// import ExcuseGeneratorHome from './execute';
+// import CategoryPage from './categoryPage';
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,8 @@ function App() {
           <Route path='/form' element={<DatingForm/>}/>
           <Route path='/' element={<HomePage/>}/>
           <Route path='/hover' element={<ReadyToDatePage/>}/>
+          {/* <Route path='/exe' element={<ExcuseGeneratorHome/>}/>
+          <Route path='/cat' element={<CategoryPage/>}/> */}
                </Routes>
       </Router>
     </div>
